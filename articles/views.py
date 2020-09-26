83f0bf208c424ed39f56c031e9f190cbdc698922
Star Liu <lltmail@163.com>
2020-09-26 09:22:21 +0800

    通用form及pagination

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Article
+
+
+class ArticleListView(ListView):
+    queryset = Article.objects.filter(status='publish')
+    context_object_name = 'articles'
+    template_name = 'articles/articles_list.html'
+    
